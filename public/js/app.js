3bbc9285e1c08a0e8e35891778ef292d3998795e
brahim houari <56647359+brhouari@users.noreply.github.com>
2024-10-06 16:45:50 +0200

    update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
-app.js
+function formToggle(ID){
+    var element = document.getElementById(ID);
+    if(element.style.display === "none"){
+        element.style.display = "block";
+    }else{
+        element.style.display = "none";
+    }
+}
